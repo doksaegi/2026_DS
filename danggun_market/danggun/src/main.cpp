f8345498ba1b0a5fbe49a330fe10677209101d44
unknown <trixy1784@kentech.ac.kr>
2026-06-06 17:53:51 +0900

    1st Commit

--- danggun_market/danggun/src/main.cpp
+++ danggun_market/danggun/src/main.cpp
@@ -1,6 +1,15 @@
 #include "Game.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     Game game;
     game.run();
     return 0;
